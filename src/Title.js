06fd682a3c030e67b8c897b2c5f7e17d18f525ba
ysawiris <youssef.sawiris@students.makeschool.com>
2020-06-02 18:49:06 -0700

    header component built

---
+++ src/Title.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Title.css';
+
+function Title() {
+    return (
+      <div className="Title">
+        <header>
+            <h1>SFPOPOS</h1>
+            <p className="Title-Subtitle">San Francisco Privately Owned Public Open Spaces</p>
+        </header>
+      </div>
+    )
+}
+
+export default Title
